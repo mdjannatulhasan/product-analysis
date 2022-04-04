9e109f85d6d31c7e93e407b7be51fa85797f0ea1
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2022-04-05 03:44:04 +0600

    404 added

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='flex justify-center'>
+            <img src="404.gif" alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
