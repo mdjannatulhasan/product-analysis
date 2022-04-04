5c2496354f7a15501b921aa18ec26c73f256846a
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2022-04-04 23:48:17 +0600

    Header with active link

---
+++ src/components/CustomLink/CustomLink.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+
+function CustomLink({ children, to, ...props }) {
+    let resolved = useResolvedPath(to);
+    let match = useMatch({ path: resolved.pathname, end: true });
+  
+    return (
+      <div>
+        <Link
+          style={{ borderBottomWidth: match ? "2px" : "0", color: match ? "#F77171" : '#000000' }}
+          to={to}
+          {...props}
+        >
+          {children}
+        </Link>
+      </div>
+    );
+  }
+export default CustomLink;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
     return (
         <div>
-            Header
+            <nav className='flex space-x-5 justify-center'>
+                <CustomLink className='hover:bg-red-200 border-red-400 py-2 px-4' to='/'>Home</CustomLink>
+                <CustomLink className='hover:bg-red-200 border-red-400 py-2 px-4' to='/reviews'>Reviews</CustomLink>
+                <CustomLink className='hover:bg-red-200 border-red-400 py-2 px-4' to='/dashboard'>Dashboard</CustomLink>
+                <CustomLink className='hover:bg-red-200 border-red-400 py-2 px-4' to='/blogs'>Blogs</CustomLink>
+                <CustomLink className='hover:bg-red-200 border-red-400 py-2 px-4' to='/about'>About</CustomLink>
+            </nav>
         </div>
     );
 };
